e756575f5dcc5f7409e1890b5109cc432768a058
Ryukyo <florian.fejer@hotmail.de>
2020-09-02 08:43:58 +0200

    App js basic routing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import avatar from './nw.png'
-
+import React from "react";
+import avatar from "./nw.png";
 
 export default function Home() {
+  return (
+    <>
+      <header>
+        <a href="#">
+          <img src={avatar} alt="avatar" style={{ width: "50px" }} />
+        </a>
+        <h2>Home page</h2>
+      </header>
 
-    return (
-        <>
-            <header>
-                <a href="#"><img src={avatar} alt="avata" style={{width: "50px"}}/></a>
-                <h2>Home page</h2>
-            </header>
-
-            <button>Find someone to talk to.</button>
-
-        </>
-    )
+      <button>Find someone to talk to.</button>
+    </>
+  );
 }
