8a1af1066fd8d30265b93240b72b7affad724942
Ryukyo <florian.fejer@hotmail.de>
2020-09-04 10:29:43 +0200

    Add crate and get users end points Co-authored by Tomoyuki

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,11 @@ const admin = require("firebase-admin");
 // init express server
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // init firestore connection
 admin.initializeApp();
@@ -17,33 +19,81 @@ const db = admin.firestore();
 // TODO need to access firestore
 
 app.get("/", (req, res) => {
-    functions.logger.log("GET /");
+  functions.logger.log("GET /");
 
-    res.status(404).send("Not Found");
+  res.status(404).send("Not Found");
 });
 
 app.get("/timestamp", (req, res) => {
-    functions.logger.log("GET /timestamp");
+  functions.logger.log("GET /timestamp");
 
-    res.json({
-        result: `${Date.now()}`
+  res.json({
+    result: `${Date.now()}`,
+  });
+});
+
+app.get("/data", async (req, res) => {
+  functions.logger.log("GET /data");
+
+  const docs = db
+    .collection("test")
+    .get()
+    .then((doc) => {
+      functions.logger.log("success, ", doc.docs[0].data());
+      return res.json(doc.docs[0].data());
+    })
+    .catch((e) => {
+      functions.logger.log("error, ", e);
+      return res.status(500).send();
     });
 });
 
-app.get("/data", async(req, res) => {
-    functions.logger.log("GET /data");
+app.post("/users", async (req, res) => {
+  functions.logger.log("POST /users");
 
-    const docs = db
-        .collection("test")
-        .get()
-        .then((doc) => {
-            functions.logger.log("success, ", doc.docs[0].data());
-            return res.json(doc.docs[0].data());
-        })
-        .catch((e) => {
-            functions.logger.log("error, ", e);
-            return res.status(500).send();
-        });
+  const doc = await db
+    .collection("users")
+    .add(
+      {
+        name: "name",
+        email: "email",
+        avatar_url: "avatar_url",
+        answers: [true, false],
+        status: "ACTIVE",
+        lang: "en",
+        friendlist: ["name1", "name2"],
+        blocklist: ["name3"],
+      },
+      { merge: true }
+    )
+    .catch((err) => {
+      functions.logger.log("err, ", err);
+      return res.status(500).send("failed");
+    });
+
+  functions.logger.log("stored, ", doc.id);
+  return res.status(201).json({ id: doc.id });
 });
 
+app.get("/users/:email", async (req, res) => {
+  functions.logger.log("GET /users/email");
+
+  const snapshot = await db
+    .collection("users")
+    .where("email", "==", req.params.email)
+    .get();
+  if (snapshot.empty) {
+    functions.logger.log("No matching documents");
+    return res.status(404).send();
+  }
+
+  const docs = [];
+  snapshot.forEach((doc) => {
+    functions.logger.log("doc, ", doc);
+    functions.logger.log("doc id, ", doc.id);
+    docs.push({ id: doc.id, ...doc.data() });
+  });
+  functions.logger.log(docs[0]);
+  return res.status(200).json(docs[0]);
+});
 exports.app = functions.https.onRequest(app);
