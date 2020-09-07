48972c937bcae6c087ebae039c0322b2456682a2
Ryukyo <florian.fejer@hotmail.de>
2020-09-07 04:58:45 +0200

    Post user endpoint chatqueue

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,9 @@ const admin = require("firebase-admin");
 const app = express();
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 // init firestore connection
@@ -18,56 +18,59 @@ const db = admin.firestore();
 // TODO need JWT token(issued by firebase auth) check before apis are used with middleware
 
 app.get("/", (req, res) => {
-    functions.logger.log("GET /");
+  functions.logger.log("GET /");
 
-    res.status(404).json({ message: "Not Found" });
+  res.status(404).json({ message: "Not Found" });
 });
 
-app.post("/users", async(req, res) => {
-    functions.logger.log("POST /users");
+// USER RELATED ENDPOINTS
+app.post("/users", async (req, res) => {
+  functions.logger.log("POST /users");
 
-    const doc = await db
-        .collection("users")
-        .add({
-            name: req.body.name,
-            email: req.body.email,
-            avatar_url: "",
-            answers: [],
-            status: "ACTIVE",
-            lang: "en",
-            friendlist: [],
-            blocklist: [],
-        }, { merge: true })
-        .catch((err) => {
-            functions.logger.log("err, ", err);
-            return res.status(500).send({
-                message: "failed",
-            });
-        });
-
-    return res.status(201).json({ id: doc.id });
-});
-
-app.get("/users/:email", async(req, res) => {
-    functions.logger.log("GET /users/email");
-
-    const snapshot = await db
-        .collection("users")
-        .where("email", "==", req.params.email)
-        .get();
-    if (snapshot.empty) {
-        functions.logger.log("No matching documents");
-        return res.status(404).send({ message: "Not Found" });
-    }
-
-    const docs = [];
-    snapshot.forEach((doc) => {
-        docs.push({ id: doc.id, ...doc.data() });
+  const doc = await db
+    .collection("users")
+    .add(
+      {
+        name: req.body.name,
+        email: req.body.email,
+        avatar_url: "",
+        answers: [],
+        status: "ACTIVE",
+        lang: "en",
+        friendlist: [],
+        blocklist: [],
+      },
+      { merge: true }
+    )
+    .catch((err) => {
+      functions.logger.log("err, ", err);
+      return res.status(500).send({
+        message: "failed",
+      });
     });
-    functions.logger.log(docs[0]);
-    return res.status(200).json(docs[0]);
+
+  return res.status(201).json({ id: doc.id });
 });
 
+app.get("/users/:email", async (req, res) => {
+  functions.logger.log("GET /users/email");
+
+  const snapshot = await db
+    .collection("users")
+    .where("email", "==", req.params.email)
+    .get();
+  if (snapshot.empty) {
+    functions.logger.log("No matching documents");
+    return res.status(404).send({ message: "Not Found" });
+  }
+
+  const docs = [];
+  snapshot.forEach((doc) => {
+    docs.push({ id: doc.id, ...doc.data() });
+  });
+  functions.logger.log(docs[0]);
+  return res.status(200).json(docs[0]);
+});
 
 app.get("/users", async (req, res) => {
   functions.logger.log("GET /users");
@@ -101,30 +104,83 @@ app.delete("/users/:id", async (req, res) => {
   }
 
   functions.logger.log(`User id ${userId} was deleted`);
-  return res.status(200).send(`User id ${userId} was deleted`);
-});
-exports.app = functions.https.onRequest(app);
-
-app.put("/users/:id", async(req, res) => {
-    functions.logger.log("PUT /users/", req.params.id);
-
-    const userId = req.params.id;
-    let body = req.body;
-
-    const targetUserDoc = db.collection("users").doc(userId);
-
-    const ref = await targetUserDoc.get();
-    if (!ref.exists) {
-        functions.logger.log("No matching entity");
-        return res.status(404).send({ message: "Not Found" });
-    }
-
-    const updatedUser = {...ref.data(), ...body };
-    await targetUserDoc.set(updatedUser);
-
-    const result = await targetUserDoc.get();
-    return res.status(200).send({ id: userId, ...result.data() });
+  return res.status(200).send({ message: `User id ${userId} was deleted` });
 });
 
-exports.app = functions.https.onRequest(app);
+app.put("/users/:id", async (req, res) => {
+  functions.logger.log("PUT /users/", req.params.id);
 
+  const userId = req.params.id;
+  let body = req.body;
+
+  const targetUserDoc = db.collection("users").doc(userId);
+
+  const ref = await targetUserDoc.get();
+  if (!ref.exists) {
+    functions.logger.log("No matching entity");
+    return res.status(404).send({ message: "Not Found" });
+  }
+
+  const updatedUser = { ...ref.data(), ...body };
+  await targetUserDoc.set(updatedUser);
+
+  const result = await targetUserDoc.get();
+  return res.status(200).send({ id: userId, ...result.data() });
+});
+
+// MATCHMAKING RELATED ENDPOINTS
+app.post("/chatqueue", async (req, res) => {
+  functions.logger.log("POST /chatqueue");
+
+  const targetUserDoc = db.collection("users").doc(req.body.id);
+
+  const ref = await targetUserDoc.get();
+  if (!ref.exists) {
+    functions.logger.log("No matching entity");
+    return res.status(404).send({ message: "Not Found" });
+  }
+
+  functions.logger.log("doc.data", ref.data());
+
+  const doc = await db
+    .collection("chatqueue")
+    .add(
+      {
+        id: req.body.id,
+        createdAt: Date.now(),
+        queueStatus: "Waiting",
+        ...ref.data(),
+      },
+      { merge: true }
+    )
+    .catch((err) => {
+      functions.logger.log("err, ", err);
+      return res.status(500).send({
+        message: "failed",
+      });
+    });
+
+  functions.logger.log("chatqueue doc", doc);
+  return res.status(201).json({ message: "success" });
+});
+
+app.get("/chatqueue/:userId", async (req, res) => {
+  functions.logger.log("GET /chatqueue/:userId");
+  const userId = req.params.id;
+
+  const snapshot = await db.collection("users").get();
+  if (snapshot.empty) {
+    functions.logger.log("No matching documents");
+    return res.status(404).send();
+  }
+
+  const docs = [];
+  snapshot.forEach((doc) => {
+    functions.logger.log("doc, ", doc);
+    functions.logger.log("doc id, ", doc.id);
+    docs.push({ id: doc.id, ...doc.data() });
+  });
+  functions.logger.log(docs);
+  return res.status(200).json(docs);
+});
+exports.app = functions.https.onRequest(app);
