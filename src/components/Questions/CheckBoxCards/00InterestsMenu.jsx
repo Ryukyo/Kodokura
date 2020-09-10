61d4e2cc9deadab12fcfb5c04af21eaf3a7eb381
Eduardo Ruiz <eduru@me.com>
2020-09-10 18:00:51 +0900

    new questions updated 50% working

---
+++ src/components/Questions/CheckBoxCards/00InterestsMenu.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { auth } from "../../../services/firebase";
+
+
+export default function InterestsMenu() {
+
+    const [hasAvatar, setHasAvatar] = useState('');
+    const user = auth().currentUser;
+
+    async function getAvatar() {
+        let req = await axios.get(`/users/${user.email}`);
+        let data = req.data;
+        let avatar = data.avatar_url;
+        console.log(data);
+        setHasAvatar(avatar);
+    }
+
+    useEffect(() => {
+        getAvatar()
+    }, []);
+
+    console.log(hasAvatar)
+
+
+    return (
+        <>
+            <h3> Interests </h3>
+            <div>
+                <Link to="/musiccard">
+                    <button>Music</button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/moviescard">
+                    <button>Movies</button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/tvshowscard">
+                    <button>TV Shows</button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/bookscard">
+                    <button>Books</button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/gamescard">
+                    <button>Games</button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/sportscard">
+                    <button>Sports</button>
+                </Link>
+            </div>
+            <div>
+                <Link to="/foodscard">
+                    <button>Foods</button>
+                </Link>
+            </div>
+            {hasAvatar.length === 0 ?
+                <Link to='/avatar'><button>Done!</button></Link> : <Link to='/profile'><button >Done!</button></Link>}
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
