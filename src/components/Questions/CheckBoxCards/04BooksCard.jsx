f1b4dc5130df8c04b115610fc38d0c2a96a69a95
Eduardo Ruiz <eduru@me.com>
2020-09-11 10:26:28 +0900

    books component updated

--- src/components/Questions/CheckBoxCards/04BooksCard.jsx
+++ src/components/Questions/CheckBoxCards/04BooksCard.jsx
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-
+import { auth } from "../../../services/firebase";
+import axios from 'axios';
 
 export default function BooksCard() {
 
+    const user = auth().currentUser;
+    const [books, setBooks] = useState();
 
+    async function getUserId() {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        let id = data.id;
+        if (books === undefined) {
+            setBooks(data.answers)
+        }
+        return id;
+    };
+    getUserId();
+
+    async function updateAnswers() {
+        const userId = await getUserId();
+        axios.put(`/users/${userId}`, { answers: books });
+    };
+
+    async function checkBox(i) {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        books.books[i] = !data.answers.books[i];
+    };
+
+    function renderCheckBox(...category) {
+        if (books) {
+            return books.books.map((e, i) => {
+                return (
+                    <div>
+                        {
+                            e ?
+                                <>
+                                    <label for="fname">{category[i]}</label>
+                                    <input type="checkbox" defaultChecked={e} onChange={() => {
+
+                                        checkBox(i)
+                                    }} />
+                                </>
+                                :
+                                <>
+                                    <label for="fname">{category[i]}</label>
+                                    <input type="checkbox" defaultChecked={e} onChange={() => {
+                                        checkBox(i)
+                                    }} />
+                                </>
+                        }
+                    </div>
+                )
+            });
+        };
+    };
 
     return (
         <>
-            <h3> What style of books do you like? </h3>
-            <input type="checkbox" value={true}>Fiction</input>
-            <input type="checkbox" value={true}>Non-fiction</input>
-            <input type="checkbox" value={true}>Drama</input>
-            <input type="checkbox" value={true}>Poetry</input>
-            <input type="checkbox" value={true}>Folktale</input>
-            <input type="checkbox" value={true}>Mystery</input>
-            <input type="checkbox" value={true}>Biography</input>
-            <input type="checkbox" value={true}>Romantic</input>
-            <input type="checkbox" value={true}>Fantasy</input>
-            <Link to="/tvshowscard">
-                <button>Previous</button>
-            </Link>
-            <Link to="/gamescard">
-                <button>Next</button>
+            <h3> What kind of books do you like? </h3>
+
+            {renderCheckBox('Fiction', 'Non-fiction', 'Fantasy', 'Drama', 'Poetry', 'Fantasy', 'Mystery', 'Biography', 'Romantic', 'Comic/Manga')}
+
+            <Link to="/interestsmenu">
+                <button onClick={() => {
+                    updateAnswers();
+                }}>Done!</button>
             </Link>
         </>
     )
