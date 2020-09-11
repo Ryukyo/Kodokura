81524df85811c82eb3f682d45b5a9caa5e352981
Eduardo Ruiz <eduru@me.com>
2020-09-11 10:22:15 +0900

    sports component updated

--- src/components/Questions/CheckBoxCards/06SportsCard.jsx
+++ src/components/Questions/CheckBoxCards/06SportsCard.jsx
@@ -1,29 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-
+import { auth } from "../../../services/firebase";
+import axios from 'axios';
 
 export default function SportsCard() {
 
+    const user = auth().currentUser;
+    const [sports, setSports] = useState();
 
+    async function getUserId() {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        let id = data.id;
+        if (sports === undefined) {
+            setSports(data.answers)
+        }
+        return id;
+    };
+    getUserId();
+
+    async function updateAnswers() {
+        const userId = await getUserId();
+        axios.put(`/users/${userId}`, { answers: sports });
+    };
+
+    async function checkBox(i) {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        sports.sports[i] = !data.answers.sports[i];
+    };
+
+    function renderCheckBox(...category) {
+        if (sports) {
+            return sports.sports.map((e, i) => {
+                return (
+                    <div>
+                        {
+                            e ?
+                                <>
+                                    <label for="fname">{category[i]}</label>
+                                    <input type="checkbox" defaultChecked={e} onChange={() => {
+
+                                        checkBox(i)
+                                    }} />
+                                </>
+                                :
+                                <>
+                                    <label for="fname">{category[i]}</label>
+                                    <input type="checkbox" defaultChecked={e} onChange={() => {
+                                        checkBox(i)
+                                    }} />
+                                </>
+                        }
+                    </div>
+                )
+            });
+        };
+    };
 
     return (
         <>
-            <h3> What kind of sports do you like? </h3>
-            <input type="checkbox" value={true}>Soccer</input>
-            <input type="checkbox" value={true}>Football</input>
-            <input type="checkbox" value={true}>Basket</input>
-            <input type="checkbox" value={true}>Tenis</input>
-            <input type="checkbox" value={true}>Ice-hockey</input>
-            <input type="checkbox" value={true}>Baseball</input>
-            <input type="checkbox" value={true}>Cycling</input>
-            <input type="checkbox" value={true}>Yoga</input>
-            <input type="checkbox" value={true}>Swimming</input>
+            <h3> What kind of sports do you like ? </h3>
 
-            <Link to="/gamescard">
-                <button>Previous</button>
-            </Link>
-            <Link to="/foodscard">
-                <button>Next</button>
+            {renderCheckBox('Soccer', 'Football', 'Basket', 'Baseball', 'Ice-hockey', 'Golf', 'Tenis', 'Cycling', 'Yoga', 'Body-building')}
+
+            <Link to="/interestsmenu">
+                <button onClick={() => {
+                    updateAnswers();
+                }}>Done!</button>
             </Link>
         </>
     )
