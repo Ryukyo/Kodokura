84c8462c637e8109bcbb33a54eceb922f1a24938
Eduardo Ruiz <eduru@me.com>
2020-09-11 10:20:48 +0900

    foods component updated

--- src/components/Questions/CheckBoxCards/07FoodsCard.jsx
+++ src/components/Questions/CheckBoxCards/07FoodsCard.jsx
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-
+import { auth } from "../../../services/firebase";
+import axios from 'axios';
 
 export default function FoodsCard() {
 
+    const user = auth().currentUser;
+    const [foods, setFoods] = useState();
 
+    async function getUserId() {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        let id = data.id;
+        if (foods === undefined) {
+            setFoods(data.answers)
+        }
+        return id;
+    };
+    getUserId();
+
+    async function updateAnswers() {
+        const userId = await getUserId();
+        axios.put(`/users/${userId}`, { answers: foods });
+    };
+
+    async function checkBox(i) {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        foods.foods[i] = !data.answers.foods[i];
+    };
+
+    function renderCheckBox(...category) {
+        if (foods) {
+            return foods.foods.map((e, i) => {
+                return (
+                    <div>
+                        {
+                            e ?
+                                <>
+                                    <label for="fname">{category[i]}</label>
+                                    <input type="checkbox" defaultChecked={e} onChange={() => {
+
+                                        checkBox(i)
+                                    }} />
+                                </>
+                                :
+                                <>
+                                    <label for="fname">{category[i]}</label>
+                                    <input type="checkbox" defaultChecked={e} onChange={() => {
+                                        checkBox(i)
+                                    }} />
+                                </>
+                        }
+                    </div>
+                )
+            });
+        };
+    };
 
     return (
         <>
-            <h3> What kind of foods do you like? </h3>
-            <input type="checkbox" value={true}>Japanese cuissine</input>
-            <input type="checkbox" value={true}>Chinese cuissine</input>
-            <input type="checkbox" value={true}>Vietnamese cuissine</input>
-            <input type="checkbox" value={true}>Mexican cuissine</input>
-            <input type="checkbox" value={true}>French cuissine</input>
-            <input type="checkbox" value={true}>Mediterranean cuissine</input>
-            <input type="checkbox" value={true}>Desserts</input>
-            <input type="checkbox" value={true}>Fast food</input>
+            <h3> What kind of foods do you like ? </h3>
 
-            <Link to="/sportscard">
-                <button>Previous</button>
-            </Link>
-            <Link to="/avatar">
-                <button>Finish</button>
+            {renderCheckBox('Japanese', 'Chinese', 'Korean', 'Vietnamese', 'Mexican', 'Italian', 'French', 'Greek', 'Desserts', 'Fast-food')}
+
+            <Link to="/interestsmenu">
+                <button onClick={() => {
+                    updateAnswers();
+                }}>Done!</button>
             </Link>
         </>
     )
