4ce0a3982ed9d81b8f5c4792f2bd85b61f155eab
tomoyuki.kobayashi <kobatommy@gmail.com>
2020-09-15 16:04:51 +0900

    age check work version / no styling

--- src/components/CreateAccount/AgeCheckDialog.jsx
+++ src/components/CreateAccount/AgeCheckDialog.jsx
@@ -5,7 +5,10 @@ import { Link } from "react-router-dom";
 //Modal.setAppElement("#root");
 
 export default function AppCheckDialog(props) {
+  const passAge = 18;
+
   const [isOpen, setIsOpen] = useState(true);
+  const [err, setErr] = useState("");
   const [year, setYear] = useState("");
   const [month, setMonth] = useState("");
   const [date, setDate] = useState("");
@@ -27,7 +30,48 @@ export default function AppCheckDialog(props) {
   }
 
   // TODO
-  function onSubmit() {}
+  function onSubmit() {
+    // current date
+    const today = new Date();
+
+    // check if input is in valid range
+    const yearInt = parseInt(year);
+    const monthInt = parseInt(month);
+    const dateInt = parseInt(date);
+    if (
+      !year ||
+      !month ||
+      !date ||
+      yearInt < 1900 ||
+      yearInt > today.getFullYear() ||
+      monthInt < 1 ||
+      monthInt > 13 ||
+      dateInt < 1 ||
+      dateInt > 31
+    ) {
+      setErr("Please enter your correct date of birth.");
+      return;
+    }
+
+    // calc age
+    const birthday = new Date(yearInt, monthInt, dateInt);
+    var age = today.getFullYear() - birthday.getFullYear();
+    var m = today.getMonth() - birthday.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+      age--;
+    }
+
+    if (age >= passAge) {
+      // if age >= 18 close dialog
+      setErr("");
+      toggleModal();
+      return;
+    } else {
+      // if age < 18 show error
+      setErr("This service is not available to anyone under the age of 18.");
+      return;
+    }
+  }
 
   return (
     <div className="App">
@@ -53,10 +97,11 @@ export default function AppCheckDialog(props) {
             onChange={handleDateChange}
           />
         </form>
-        <button onClick={toggleModal}>OK</button>
+        <button onClick={onSubmit}>OK</button>
         <Link to="/login" className="text-link">
           <button>Cancel</button>
         </Link>
+        {err ? <div>{err}</div> : <div />}
       </Modal>
     </div>
   );
