47962d5a0479fc866514013cf5c859a4d0184df8
tomoyuki.kobayashi <kobatommy@gmail.com>
2020-09-15 15:19:30 +0900

    WIP

--- src/components/CreateAccount/AgeCheckDialog.jsx
+++ src/components/CreateAccount/AgeCheckDialog.jsx
@@ -1,21 +1,62 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import { Link } from "react-router-dom";
 
 //Modal.setAppElement("#root");
 
-export default function AppCheckDialog() {
+export default function AppCheckDialog(props) {
   const [isOpen, setIsOpen] = useState(true);
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+  const [date, setDate] = useState("");
 
   function toggleModal() {
     setIsOpen(!isOpen);
   }
 
+  function handleYearChange(event) {
+    setYear(event.target.value);
+  }
+
+  function handleMonthChange(event) {
+    setMonth(event.target.value);
+  }
+
+  function handleDateChange(event) {
+    setDate(event.target.value);
+  }
+
+  // TODO
+  function onSubmit() {}
+
   return (
     <div className="App">
       <Modal isOpen={isOpen} contentLabel="age check dialog">
         <div>Enter your date of birth</div>
+        <form>
+          <input
+            type="number"
+            name="year"
+            placeholder="2000"
+            onChange={handleYearChange}
+          />
+          <input
+            type="number"
+            name="month"
+            placeholder="1"
+            onChange={handleMonthChange}
+          />
+          <input
+            type="number"
+            name="date"
+            placeholder="31"
+            onChange={handleDateChange}
+          />
+        </form>
         <button onClick={toggleModal}>OK</button>
-        <button onClick={toggleModal}>Cancel</button>
+        <Link to="/login" className="text-link">
+          <button>Cancel</button>
+        </Link>
       </Modal>
     </div>
   );
