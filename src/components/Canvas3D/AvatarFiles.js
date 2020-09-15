2ea2c54d1528325eafbaee35de6016ddb6f395d1
TwenLeMammouth <60606340+TwenLeMammouth@users.noreply.github.com>
2020-09-15 10:01:15 +0900

    3D Avatars + cubes for loading

---
+++ src/components/Canvas3D/AvatarFiles.js
@@ -0,0 +1,43 @@
+import turtle3D from "./TurtleShellMeshPBR.glb"
+import turtleTexture from "./PBRTurtleShellComplete.png"
+import slime3D from "./SlimeMeshPBR.glb"
+import slimeTexture from "./PBRSlimeComplete.png"
+
+export const AvatarFiles = { 
+                Turtle:{
+                    name: "Turtle",
+                    object3D: turtle3D,
+                    texture: turtleTexture
+                },
+                Slime:{
+                    name:"Slime",
+                    object3D: slime3D,
+                    texture: slimeTexture
+                },
+            }
+                    // Bear:{
+                    //    name:"Bear",
+                    //     object3D: bear3D,
+                    //     texture: animal1Texture
+                    // },
+                    // Bird:{
+                    //    name:"Bird",
+                    //     object3D: bird3D,
+                    //     texture: animal1Texture
+                    // },
+                    // Bunny:{
+                    //    name:"Bunny",
+                    //     object3D: bunny3D,
+                    //     texture: animal1Texture
+                    // },
+                    // Slime:{
+                    //    name:"Slime",
+                    //     object3D: slime3D,
+                    //     texture: animal1Texture
+                    // },
+                    // Slime:{
+                    //     object3D: slime3D,
+                    //     texture: animal1Texture
+                    // },
+
+ 
