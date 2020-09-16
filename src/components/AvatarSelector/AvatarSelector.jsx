45912135fa9830fa83926edbf20a081b6ec93711
tomoyuki.kobayashi <kobatommy@gmail.com>
2020-09-16 13:38:43 +0900

    WIP

--- src/components/AvatarSelector/AvatarSelector.jsx
+++ src/components/AvatarSelector/AvatarSelector.jsx
@@ -1,69 +1,61 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import bulbasaur from './img/bulbasaur.png';
+import charmander from './img/charmander.png';
+import pikachu from './img/pikachu.png';
+import raichu from './img/raichu.png';
+import squirtle from './img/squirtle.png';
+import togepi from './img/togepi.png';
+import axios from 'axios';
 
 import { auth } from "../../services/firebase";
-import {
-  getUser,
-  updateAvatar as updateAvatarReq,
-} from "../../helpers/backend";
+import { Link } from 'react-router-dom';
 
-import bulbasaur from "./img/bulbasaur.png";
-import charmander from "./img/charmander.png";
-import pikachu from "./img/pikachu.png";
-import raichu from "./img/raichu.png";
-import squirtle from "./img/squirtle.png";
-import togepi from "./img/togepi.png";
 
 export default function Avatar() {
-  const authUser = auth().currentUser;
 
-  async function updateAvatar(avatar) {
-    const user = await getUser(authUser.email);
-    updateAvatarReq(user.id, avatar);
-  }
+    const user = auth().currentUser;
+    async function getUserId() {
+        let req = await axios.get(`/users/${user.email}`)
+        let data = req.data;
+        let id = data.id;
+        console.log(data.avatar_url)
+        return id;
+    };
 
-  return (
-    <>
-      <div className="avatar-selector">
-        <header>
-          <p>Choose your avatar</p>
-        </header>
+    async function updateAvatar(avatar) {
+        const userId = await getUserId();
+        axios.put(`/users/${userId}`, {'avatar_url': avatar});
+    };
 
-        <section className="avatar">
-          <img
-            src={bulbasaur}
-            alt="avatar"
-            onClick={() => updateAvatar(bulbasaur)}
-          />
-          <img
-            src={charmander}
-            alt="avatar"
-            onClick={() => updateAvatar(charmander)}
-          />
-          <img
-            src={pikachu}
-            alt="avatar"
-            onClick={() => updateAvatar(pikachu)}
-          />
-          <img src={raichu} alt="avatar" onClick={() => updateAvatar(raichu)} />
-          <img
-            src={squirtle}
-            alt="avatar"
-            onClick={() => updateAvatar(squirtle)}
-          />
-          <img src={togepi} alt="avatar" onClick={() => updateAvatar(togepi)} />
-        </section>
+    return (
+        <>
+            <div className="avatar-selector">
 
-        <nav className="btn">
-          <Link to="/home">
-            <button>Start chatting!</button>
-          </Link>
+                <header>
+                    <p>Choose your avatar</p>
+                </header>
+                
+                <section className="avatar">
+                    <img src={bulbasaur} alt="avatar" onClick={() => updateAvatar(bulbasaur)}/>
+                    <img src={charmander} alt="avatar" onClick={() => updateAvatar(charmander)}/>
+                    <img src={pikachu} alt="avatar" onClick={() => updateAvatar(pikachu)}/>
+                    <img src={raichu} alt="avatar" onClick={() => updateAvatar(raichu)}/>
+                    <img src={squirtle} alt="avatar" onClick={() => updateAvatar(squirtle)}/>
+                    <img src={togepi} alt="avatar" onClick={() => updateAvatar(togepi)}/>
+                </section>
 
-          <Link to="/profile">
-            <button>Go to profile</button>
-          </Link>
-        </nav>
-      </div>
-    </>
-  );
+                <nav className="btn">
+                    <Link to='/home'>
+                        <button>Start chatting!</button>
+                    </Link>
+                    
+                    <Link to='/profile'>
+                        <button>Go to profile</button>
+                    </Link>
+                </nav>
+            </div>
+            
+
+        </>
+    )
 }
